c30802c13de02f2930d81576d4d18f0fb2cd75ba
Josue Sanchez Salazar <156948878+DavidSalazar052@users.noreply.github.com>
2026-03-12 12:33:13 -0600

    Commit de Inicio

---
+++ src/main/java/com/example/proyecto_p4_bolsaempleo/ProyectoP4BolsaEmpleoApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_p4_bolsaempleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoP4BolsaEmpleoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoP4BolsaEmpleoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyecto_p4_bolsaempleo/ProyectoP4BolsaEmpleoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_p4_bolsaempleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoP4BolsaEmpleoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
