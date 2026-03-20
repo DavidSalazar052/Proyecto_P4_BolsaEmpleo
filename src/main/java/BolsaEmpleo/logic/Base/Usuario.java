0c809ac9857fedea272ae7596ba9b46df94a3157
Josue Sanchez Salazar <156948878+DavidSalazar052@users.noreply.github.com>
2026-03-20 15:35:57 -0600

    BD arregladas

--- src/main/java/BolsaEmpleo/logic/Base/Usuario.java
+++ src/main/java/BolsaEmpleo/logic/Base/Usuario.java
@@ -1,24 +1,26 @@
 package BolsaEmpleo.logic.Base;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "usuario")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 
 
-public abstract class Usuario {
+public class Usuario {
     @Id
     private String id;
-
+    
     private String username;
     private String clave;
     private String tipo;
 }
+
+
